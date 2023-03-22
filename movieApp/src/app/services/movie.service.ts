b7899ff5383226e358a3b359f803ab75e4d6434b
AKeveri <aleksi.keveri@gmail.com>
2023-03-22 20:09:37 +0200

    f

--- movieApp/src/app/services/movie.service.ts
+++ movieApp/src/app/services/movie.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTopRatedMovies() {}
+
+  getMovieDetails() {}
 }
